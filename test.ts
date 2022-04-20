f52bd70159bd2d7eea8add1fd4c00e44c6309c1c
Mickey1992 <wl920425@gmail.com>
2022-04-20 23:16:16 +0900

    open the resource pages one by one, and get their hashcode

--- test.ts
+++ test.ts
@@ -1,31 +1,50 @@
+import { group } from 'console';
 import * as https from 'https';
 import { JSDOM } from 'jsdom';
 import { Readable } from 'stream';
 import { createGunzip } from 'zlib';
 
-https.get("https://nyaa.si/", response => {
-    let output: Readable = response;
+const baseURL: string = "https://nyaa.si" ;
 
-    if (response.headers['content-encoding'] === "gzip") {
-        const gunzip = createGunzip();
-        response.pipe(gunzip);
-        output = gunzip;
+getInfo(baseURL, document => {
+    const nodes = document.querySelectorAll("a[href^=\\/view]:not(.comments)");
+    getHashInfo(nodes, 0);
+});
+
+
+function getHashInfo(nodes: NodeListOf<Element>, index: number){
+    if(index < nodes.length){
+        const link = baseURL + nodes[index].getAttribute("href");
+        getInfo(link, document => {
+            const hashCode = document.querySelector("kbd")?.textContent;
+            console.info(`${nodes[index].textContent} : link = ${link}, hashCode = ${hashCode}`);
+            getHashInfo(nodes, index+1);
+        })
     }
-
-    const chunks: any[] = [];
-    output.on("data", d => {
-        chunks.push(d);
-    });
-    output.on("end", () => {
-        const html: string = Buffer.concat(chunks).toString();
-        const nodes = new JSDOM(html).window.document.querySelectorAll("tbody > tr");
-        nodes.forEach(n => {
-            const title = n.querySelector("a[href^=\\/view]")?.textContent;
-            console.info(title);
-            const link = n.querySelector("a[href^=\\magnet]")?.getAttribute("href");
-            console.info(link);
-            // console.info(`${n.textContent}: ${n.getAttribute("href")}`);
-        });
-    })
     
-})
+}
+
+function getInfo(url: string, onRetrieved: (document: Document) => void){
+    https.get(url, response => {
+        let output: Readable = response;
+    
+        if (response.headers['content-encoding'] === "gzip") {
+            const gunzip = createGunzip();
+            response.pipe(gunzip);
+            output = gunzip;
+        }
+    
+        const chunks: any[] = [];
+        output.on("data", d => {
+            chunks.push(d);
+        });
+
+        
+        output.on("end", () => {
+            const document = new JSDOM(Buffer.concat(chunks).toString()).window.document;
+            onRetrieved(document);
+        })
+    })
+}
+
+
