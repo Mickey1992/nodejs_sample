3b9278260ac00cb755d55f9612fbb484651d6b2a
Mickey1992 <wl920425@gmail.com>
2022-04-21 21:54:40 +0900

    rewrite by using promise

--- test.ts
+++ test.ts
@@ -1,50 +1,72 @@
 import { group } from 'console';
 import * as https from 'https';
 import { JSDOM } from 'jsdom';
+import { resolve } from 'path';
 import { Readable } from 'stream';
 import { createGunzip } from 'zlib';
 
 const baseURL: string = "https://nyaa.si" ;
 
-getInfo(baseURL, document => {
-    const nodes = document.querySelectorAll("a[href^=\\/view]:not(.comments)");
-    getHashInfo(nodes, 0);
-});
+
+// getInfo(baseURL, document => {
+//     const nodes = document.querySelectorAll("a[href^=\\/view]:not(.comments)");
+//     getHashInfo(nodes, 0);
+// });
 
 
-function getHashInfo(nodes: NodeListOf<Element>, index: number){
-    if(index < nodes.length){
-        const link = baseURL + nodes[index].getAttribute("href");
-        getInfo(link, document => {
-            const hashCode = document.querySelector("kbd")?.textContent;
-            console.info(`${nodes[index].textContent} : link = ${link}, hashCode = ${hashCode}`);
-            getHashInfo(nodes, index+1);
-        })
+// function getHashInfo(nodes: NodeListOf<Element>, index: number){
+//     if(index < nodes.length){
+//         const link = baseURL + nodes[index].getAttribute("href");
+//         getInfo(link, document => {
+//             const hashCode = document.querySelector("kbd")?.textContent;
+//             console.info(`${nodes[index].textContent} : link = ${link}, hashCode = ${hashCode}`);
+//             getHashInfo(nodes, index+1);
+//         })
+//     }
+    
+// }
+
+getInfo(baseURL).then(document => {
+    return document.querySelectorAll("a[href^=\\/view]:not(.comments)");
+}).then(nodes => {
+    console.info(nodes.length);
+    let promise = new Promise<void>((resolve, reject) => {resolve();});
+    for (const node of nodes){
+        const link = baseURL + node.getAttribute("href");
+        promise = promise.then(() => {
+            //console.info(link);
+            return getInfo(link);
+        }).then(d => {
+            const hashCode = d.querySelector("kbd")?.textContent;
+            console.info(`${node.textContent} : link = ${link}, hashCode = ${hashCode}`);
+        });     
     }
-    
-}
+})
 
-function getInfo(url: string, onRetrieved: (document: Document) => void){
-    https.get(url, response => {
-        let output: Readable = response;
-    
-        if (response.headers['content-encoding'] === "gzip") {
-            const gunzip = createGunzip();
-            response.pipe(gunzip);
-            output = gunzip;
-        }
-    
-        const chunks: any[] = [];
-        output.on("data", d => {
-            chunks.push(d);
-        });
-
+function getInfo(url: string): Promise<Document>{
+    return new Promise<Document>((resolve, reject) =>{
+        https.get(url, response => {
+            let output: Readable = response;
         
-        output.on("end", () => {
-            const document = new JSDOM(Buffer.concat(chunks).toString()).window.document;
-            onRetrieved(document);
+            if (response.headers['content-encoding'] === "gzip") {
+                const gunzip = createGunzip();
+                response.pipe(gunzip);
+                output = gunzip;
+            }
+        
+            const chunks: any[] = [];
+            output.on("data", d => {
+                chunks.push(d);
+            });
+    
+            
+            output.on("end", () => {
+                const document = new JSDOM(Buffer.concat(chunks).toString()).window.document;
+                resolve(document);
+            })
         })
-    })
+    });
+    
 }
 
 
