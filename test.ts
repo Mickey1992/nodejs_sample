a183dc454a7ee03b20122d1437ac59c34eb03aca
Mickey1992 <wl920425@gmail.com>
2022-04-27 00:49:30 +0900

    rewrite with async/await

--- test.ts
+++ test.ts
@@ -5,45 +5,21 @@ import { resolve } from 'path';
 import { Readable } from 'stream';
 import { createGunzip } from 'zlib';
 
-const baseURL: string = "https://nyaa.si" ;
+getHashCode();
 
-
-// getInfo(baseURL, document => {
-//     const nodes = document.querySelectorAll("a[href^=\\/view]:not(.comments)");
-//     getHashInfo(nodes, 0);
-// });
-
-
-// function getHashInfo(nodes: NodeListOf<Element>, index: number){
-//     if(index < nodes.length){
-//         const link = baseURL + nodes[index].getAttribute("href");
-//         getInfo(link, document => {
-//             const hashCode = document.querySelector("kbd")?.textContent;
-//             console.info(`${nodes[index].textContent} : link = ${link}, hashCode = ${hashCode}`);
-//             getHashInfo(nodes, index+1);
-//         })
-//     }
-    
-// }
-
-getInfo(baseURL).then(document => {
-    return document.querySelectorAll("a[href^=\\/view]:not(.comments)");
-}).then(nodes => {
-    console.info(nodes.length);
-    let promise = new Promise<void>((resolve, reject) => {resolve();});
-    for (const node of nodes){
+async function getHashCode() {
+    const baseURL: string = "https://nyaa.si" ;
+    let document = await getDocument(baseURL);
+    let nodes = document.querySelectorAll("a[href^=\\/view]:not(.comments)");
+    for (const node of nodes) {
         const link = baseURL + node.getAttribute("href");
-        promise = promise.then(() => {
-            //console.info(link);
-            return getInfo(link);
-        }).then(d => {
-            const hashCode = d.querySelector("kbd")?.textContent;
-            console.info(`${node.textContent} : link = ${link}, hashCode = ${hashCode}`);
-        });     
+        let d = await getDocument(link);
+        const hashCode = d.querySelector("kbd")?.textContent;
+        console.info(`${node.textContent} : link = ${link}, hashCode = ${hashCode}`);
     }
-})
+}
 
-function getInfo(url: string): Promise<Document>{
+function getDocument(url: string): Promise<Document>{
     return new Promise<Document>((resolve, reject) =>{
         https.get(url, response => {
             let output: Readable = response;
